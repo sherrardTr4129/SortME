f2dacfd9688131db09c02839452587ac91bd73f3
sherrardtr4129 <tws4129@g.rit.edu>
2014-01-28 14:25:01 -0500

    changed angledrive.ino to handle inverted motors

--- arduino code/AngleDrive/AngleDrive.ino
+++ arduino code/AngleDrive/AngleDrive.ino
@@ -1,7 +1,7 @@
 #include <AFMotor.h>
 
 /*
-* Mecanum drive method for SortME V2 robot
+ * Mecanum drive method for SortME V2 robot
  * Author: Trevor Sherrard
  * using the AFMotor library found here:
  * https://github.com/adafruit/Adafruit-Motor-Shield-library
@@ -27,18 +27,23 @@ void setup()
 
 void loop()
 {
+  getSetRun(45);
+}
+
+void getSetRun(double theta)
+{
   double speed1, speed2, speed3, speed4 = 0;
-  speed1 = getSpeedArray(30, 1, 1);
-  speed2 = getSpeedArray(30, 1, 2);
-  speed3 = getSpeedArray(30, 1, 3);
-  speed4 = getSpeedArray(30, 1, 4);
+  speed1 = getSpeedArray(theta, 1, 1);
+  speed2 = getSpeedArray(theta, 1, 2);
+  speed3 = getSpeedArray(theta, 1, 3);
+  speed4 = getSpeedArray(theta, 1, 4);
   Serial.print(speed1);
 
   speed1 = modifiedMap(speed1, -1, 1, -255, 255);
   speed2 = modifiedMap(speed2, -1, 1, -255, 255);
   speed3 = modifiedMap(speed3, -1, 1, -255, 255);
   speed4 = modifiedMap(speed4, -1, 1, -255, 255);
-  
+
   int speedArr[4];
   speedArr[0] = speed1;
   speedArr[1] = speed2;
@@ -49,14 +54,14 @@ void loop()
   if(checkReverse1(speedArr[0]))
   {
     m1.setSpeed(abs(speedArr[0]));
-    m1.run(BACKWARD);
+    m1.run(FORWARD);
   }
   else
   {
     m1.setSpeed(speedArr[0]);
-    m1.run(FORWARD);
+    m1.run(BACKWARD);
   }
-  
+
   if(checkReverse1(speedArr[1]))
   {
     m2.setSpeed(abs(speedArr[1]));
@@ -67,7 +72,7 @@ void loop()
     m2.setSpeed(speedArr[1]);
     m2.run(FORWARD);
   }
-    if(checkReverse1(speedArr[2]))
+  if(checkReverse1(speedArr[2]))
   {
     m3.setSpeed(abs(speedArr[2]));
     m3.run(BACKWARD);
@@ -77,15 +82,15 @@ void loop()
     m3.setSpeed(speedArr[2]);
     m3.run(FORWARD);
   }
-    if(checkReverse1(speedArr[3]))
+  if(checkReverse1(speedArr[3]))
   {
     m4.setSpeed(abs(speedArr[3]));
-    m4.run(BACKWARD);
+    m4.run(FORWARD);
   }
   else
   {
     m4.setSpeed(speedArr[3]);
-    m4.run(FORWARD);
+    m4.run(BACKWARD);
   }
   delay(1000);
 }
@@ -147,5 +152,6 @@ double getSpeedArray(double angle, double magnitude, int wheelnumber)
   }
   return motorSpeed;
 }
+
 
 
